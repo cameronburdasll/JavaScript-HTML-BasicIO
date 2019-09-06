9cb1c8e6debbeba121cb6b30ea41b198584e917c
cameronburdsall <54965721+cameronburdsall@users.noreply.github.com>
2019-09-05 17:22:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function loopPrint (numLoops)
+{
+    var text = document.getElementById('outputWrapper');
+    var output = '';
+    for (let i = 0; i < j; i ++)
+        {
+            console.log(i);
+            output += '<span class="outputstream">Output loop #' + (i + 1) + '</span><br/><br/>';
+        }
+    text.innerHTML = output;
+}
+function main() 
+{
+    
+    //reads sequence of numbers input
+    var x = document.getElementById("in1").value;
+    x = Array.prototype.slice.call(x);
+    //reads number of loops input
+    var numLoops = document.getElementById("loops").value;
+    output = x.sort()
+    //this will display the sorted sequence
+    document.getElementById("output").innerHTML = output;
+    //this calls loopPrint which will print a statement numLoops times
+    loopPrint(numLoops);
+}
